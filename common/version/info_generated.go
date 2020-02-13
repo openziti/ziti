c63c0e70924e94320d93855e66bb10a9d3ef8d15
Travis CI User <travis@example.org>
2020-02-13 20:07:08 +0000

    Release v0.9.2

--- common/version/info_generated.go
+++ common/version/info_generated.go
@@ -3,9 +3,9 @@
 package version
 
 const (
-	Version   = "v0.9.0"
-	Revision  = "ea556fc18740"
-	Branch    = "travis-build"
-	BuildUser = "Paul Lorenz"
-	BuildDate = "2020-02-11 16:09:08"
+	Version   = "v0.9.2"
+	Revision  = "3e48acdc7d7c"
+	Branch    = "master"
+	BuildUser = "Travis CI User"
+	BuildDate = "2020-02-13 20:07:08"
 )
