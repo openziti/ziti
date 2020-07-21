1bd7d54e2b18fa98d4a687a235bd3e27a421690a
ziti-ci <ziti-ci@netfoundry.io>
2020-07-21 15:16:47 +0000

    Release v0.15.2

--- common/version/info_generated.go
+++ common/version/info_generated.go
@@ -3,9 +3,9 @@
 package version
 
 const (
-	Version   = "v0.9.0"
-	Revision  = "ea556fc18740"
-	Branch    = "travis-build"
-	BuildUser = "Paul Lorenz"
-	BuildDate = "2020-02-11 16:09:08"
+	Version   = "v0.15.2"
+	Revision  = "2d886ff19c7a"
+	Branch    = "master"
+	BuildUser = "Travis CI User"
+	BuildDate = "2020-07-21 15:16:47"
 )
