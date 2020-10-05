892b53f84fbb731130f847ff49fee5d4f6b84dea
rentallect <curt.tudor@netfoundry.io>
2020-10-05 11:42:49 -0400

    checkpoint

--- ziti-router/main.go
+++ ziti-router/main.go
@@ -24,6 +24,8 @@ import (
 	"github.com/openziti/foundation/transport/tcp"
 	"github.com/openziti/foundation/transport/tls"
 	"github.com/openziti/foundation/transport/transwarp"
+	"github.com/openziti/foundation/transport/ws"
+	"github.com/openziti/foundation/transport/wss"
 	"github.com/openziti/ziti/common/version"
 	"github.com/openziti/ziti/ziti-router/subcmd"
 	"github.com/sirupsen/logrus"
@@ -36,6 +38,8 @@ func init() {
 	transport.AddAddressParser(tls.AddressParser{})
 	transport.AddAddressParser(tcp.AddressParser{})
 	transport.AddAddressParser(transwarp.AddressParser{})
+	transport.AddAddressParser(ws.AddressParser{})
+	transport.AddAddressParser(wss.AddressParser{})
 
 	build.InitBuildInfo(version.GetCmdBuildInfo())
 }
