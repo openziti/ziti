openziti/fabric

Initial stab at session events

events/global_registry.go
@@ -2,6 +2,7 @@ package events
 
 import (
 	"fmt"
+	"github.com/michaelquigley/pfxlog"
 	"github.com/pkg/errors"
 	"strings"
 	"sync"
@@ -25,26 +26,36 @@ type eventHandlerConfig struct {
 
 func (eventHandlerConfig *eventHandlerConfig) createHandler() (interface{}, error) {
 	handlerVal, ok := eventHandlerConfig.config["handler"]
+
+	logger := pfxlog.Logger()
+
 	if !ok {
+		logger.Errorf("no event handler defined for %v", eventHandlerConfig.id)
 		return nil, errors.Errorf("no event handler defined for %v", eventHandlerConfig.id)
 	}
 
 	handlerMap, ok := handlerVal.(map[interface{}]interface{})
 	if !ok {
+		logger.Errorf("event configuration for %v is not a map", eventHandlerConfig.id)
 		return nil, errors.Errorf("event configuration for %v is not a map", eventHandlerConfig.id)
 	}
 
-	handlerTypeVal, ok := handlerMap["type"]
+	// handlerTypeVal, ok := handlerMap["type"]
 	if !ok {
+		logger.Errorf("no handler type for %v provided", eventHandlerConfig.id)
 		return nil, errors.Errorf("no handler type for %v provided", eventHandlerConfig.id)
 	}
 
-	handlerType := fmt.Sprintf("%v", handlerTypeVal)
+	//handlerType := fmt.Sprintf("%v", handlerTypeVal)
+	//logger.Infof("Create handler of type: %s", handlerType)
 
-	handlerFactory, ok := eventHandlerTypeFactories[handlerType]
-	if !ok {
-		return nil, errors.Errorf("invalid handler type %v for handler %v provided", handlerType, eventHandlerConfig.id)
-	}
+	// handlerFactory, ok := eventHandlerTypeFactories[handlerType]
+	handlerFactory, ok  := registerJsonLoggerEventHandlerType(handlerMap)
+
+	//if !ok {
+	//	logger.Errorf("invalid handler type %v for handler %v provided", handlerType, eventHandlerConfig.id)
+	//	return nil, errors.Errorf("invalid handler type %v for handler %v provided", handlerType, eventHandlerConfig.id)
+	//}
 
 	return handlerFactory.NewEventHandler(handlerMap)
 }
@@ -55,7 +66,10 @@ func (eventHandlerConfig *eventHandlerConfig) processSubscriptions(handler inter
 		return err
 	}
 
+	logger := pfxlog.Logger()
+
 	subs, ok := eventHandlerConfig.config["subscriptions"]
+
 	if !ok {
 		return errors.Errorf("event handler %v doesn't define any subscriptions", eventHandlerConfig.id)
 	}
@@ -71,21 +85,28 @@ func (eventHandlerConfig *eventHandlerConfig) processSubscriptions(handler inter
 			return errors.Errorf("The subscription at index %v for event handler %v is not a map", idx, eventHandlerConfig.id)
 		}
 		eventTypeVal, ok := subMap["type"]
+
 		if !ok {
 			return errors.Errorf("The subscription at index %v for event handler %v has no type", idx, eventHandlerConfig.id)
 		}
 
+		logger.Infof("Processing subscriptions for event type: %s", eventTypeVal)
 		eventType := fmt.Sprintf("%v", eventTypeVal)
 
 		if registrationHandler, ok := eventTypes[eventType]; ok {
 			if err := registrationHandler(handler, subMap); err != nil {
+				logger.Error("Failed to register event handler, %v", err)
 				return err
+			}else {
+				logger.Infof("Registration of event handler %s succeeded", eventTypeVal)
 			}
 		} else {
 			var validTypes []string
 			for k := range eventTypes {
 				validTypes = append(validTypes, k)
 			}
+
+			logger.Errorf("invalid event type %v. valid types are %v", eventType, strings.Join(validTypes, ","))
 			return errors.Errorf("invalid event type %v. valid types are %v", eventType, strings.Join(validTypes, ","))
 		}
 	}
@@ -98,11 +119,11 @@ func RegisterEventType(eventType string, registrationHandler registrationHandler
 	eventTypes[eventType] = registrationHandler
 }
 
-func RegisterEventHandlerType(eventHandlerType string, factory EventHandlerFactory) {
-	registryLock.Lock()
-	defer registryLock.Unlock()
-	eventHandlerTypeFactories[eventHandlerType] = factory
-}
+//func RegisterEventHandlerType(eventHandlerType string, factory func(config map[interface{}]interface{}) EventHandlerFactory) {
+//	registryLock.Lock()
+//	defer registryLock.Unlock()
+//	eventHandlerTypeFactories[eventHandlerType] = factory
+//}
 
 func RegisterEventHandler(id interface{}, config map[interface{}]interface{}) {
 	registryLock.Lock()
@@ -136,20 +157,24 @@ events:
 func WireEventHandlers() error {
 	registryLock.Lock()
 	defer registryLock.Unlock()
-
+	logger := pfxlog.Logger()
 	for _, eventHandlerConfig := range eventHandlerConfigs {
 		handler, err := eventHandlerConfig.createHandler()
 		if err != nil {
+			logger.Errorf("Unable to create event handler: %s", err)
 			return err
 		}
 		if err := eventHandlerConfig.processSubscriptions(handler); err != nil {
+			logger.Errorf("Unable to process subscription for event handler: %s", err)
 			return err
 		}
 	}
 
-	eventTypes = nil
-	eventHandlerTypeFactories = nil
-	eventHandlerConfigs = nil
+	//eventTypes = nil
+	//eventHandlerTypeFactories = nil
+	//eventHandlerConfigs = nil
 
 	return nil
 }
+
+

events/registrations.go
@@ -4,22 +4,23 @@ import (
 	"github.com/openziti/fabric/controller/network"
 	"github.com/openziti/fabric/trace"
 	"github.com/openziti/foundation/util/cowslice"
-	"github.com/pkg/errors"
-	"reflect"
 )
 
 func init() {
-	RegisterEventType("fabric.sessions", registerSessionEventHandler)
+
+	RegisterEventType("fabric.session", registerSessionEventHandler)
 	RegisterEventType("metrics", registerMetricsEventHandler)
 
-	RegisterEventType("fabric.traces", func(val interface{}, _ map[interface{}]interface{}) error {
-		handler, ok := val.(trace.EventHandler)
-		if !ok {
-			return errors.Errorf("type %v doesn't implement github.com/openziti/fabric/trace/EventHandler interface.", reflect.TypeOf(val))
-		}
-		AddTraceEventHandler(handler)
-		return nil
-	})
+	// RegisterEventType("fabric.session", registerJsonLoggerEventHandlerType)
+
+	//RegisterEventType("fabric.traces", func(val interface{}, _ map[interface{}]interface{}) error {
+	//	handler, ok := val.(trace.EventHandler)
+	//	if !ok {
+	//		return errors.Errorf("type %v doesn't implement github.com/openziti/fabric/trace/EventHandler interface.", reflect.TypeOf(val))
+	//	}
+	//	AddTraceEventHandler(handler)
+	//	return nil
+	//})
 }
 
 func AddSessionEventHandler(handler network.SessionEventHandler) {

openziti/ziti

events/global_registry.go
@@ -2,6 +2,7 @@ package events
 
 import (
 	"fmt"
+	"github.com/michaelquigley/pfxlog"
 	"github.com/pkg/errors"
 	"strings"
 	"sync"
@@ -25,24 +26,35 @@ type eventHandlerConfig struct {
 
 func (eventHandlerConfig *eventHandlerConfig) createHandler() (interface{}, error) {
 	handlerVal, ok := eventHandlerConfig.config["handler"]
+
+	logger := pfxlog.Logger()
+
 	if !ok {
+		logger.Errorf("no event handler defined for %v", eventHandlerConfig.id)
 		return nil, errors.Errorf("no event handler defined for %v", eventHandlerConfig.id)
 	}
 
 	handlerMap, ok := handlerVal.(map[interface{}]interface{})
 	if !ok {
+		logger.Errorf("event configuration for %v is not a map", eventHandlerConfig.id)
 		return nil, errors.Errorf("event configuration for %v is not a map", eventHandlerConfig.id)
 	}
 
 	handlerTypeVal, ok := handlerMap["type"]
 	if !ok {
+		logger.Errorf("no handler type for %v provided", eventHandlerConfig.id)
 		return nil, errors.Errorf("no handler type for %v provided", eventHandlerConfig.id)
 	}
 
 	handlerType := fmt.Sprintf("%v", handlerTypeVal)
+	logger.Infof("Create handler of type: %s", handlerType)
 
-	handlerFactory, ok := eventHandlerTypeFactories[handlerType]
+	// @FIXME - fix factory loader
+	// handlerFactory, ok := eventHandlerTypeFactories[handlerType]
+	handlerFactory, ok  := registerFileLoggerEventHandlerType(handlerMap)
+
 	if !ok {
+		logger.Errorf("invalid handler type %v for handler %v provided", handlerType, eventHandlerConfig.id)
 		return nil, errors.Errorf("invalid handler type %v for handler %v provided", handlerType, eventHandlerConfig.id)
 	}
 
@@ -55,7 +67,10 @@ func (eventHandlerConfig *eventHandlerConfig) processSubscriptions(handler inter
 		return err
 	}
 
+	logger := pfxlog.Logger()
+
 	subs, ok := eventHandlerConfig.config["subscriptions"]
+
 	if !ok {
 		return errors.Errorf("event handler %v doesn't define any subscriptions", eventHandlerConfig.id)
 	}
@@ -71,21 +86,28 @@ func (eventHandlerConfig *eventHandlerConfig) processSubscriptions(handler inter
 			return errors.Errorf("The subscription at index %v for event handler %v is not a map", idx, eventHandlerConfig.id)
 		}
 		eventTypeVal, ok := subMap["type"]
+
 		if !ok {
 			return errors.Errorf("The subscription at index %v for event handler %v has no type", idx, eventHandlerConfig.id)
 		}
 
+		logger.Infof("Processing subscriptions for event type: %s", eventTypeVal)
 		eventType := fmt.Sprintf("%v", eventTypeVal)
 
 		if registrationHandler, ok := eventTypes[eventType]; ok {
 			if err := registrationHandler(handler, subMap); err != nil {
+				logger.Errorf("Failed to register event handler, %v", err)
 				return err
+			}else {
+				logger.Infof("Registration of event handler %s succeeded", eventTypeVal)
 			}
 		} else {
 			var validTypes []string
 			for k := range eventTypes {
 				validTypes = append(validTypes, k)
 			}
+
+			logger.Errorf("invalid event type %v. valid types are %v", eventType, strings.Join(validTypes, ","))
 			return errors.Errorf("invalid event type %v. valid types are %v", eventType, strings.Join(validTypes, ","))
 		}
 	}
@@ -98,11 +120,12 @@ func RegisterEventType(eventType string, registrationHandler registrationHandler
 	eventTypes[eventType] = registrationHandler
 }
 
-func RegisterEventHandlerType(eventHandlerType string, factory EventHandlerFactory) {
-	registryLock.Lock()
-	defer registryLock.Unlock()
-	eventHandlerTypeFactories[eventHandlerType] = factory
-}
+// func RegisterEventHandlerType(eventHandlerType string, factory func(config map[interface{}]interface{}) EventHandlerFactory) {
+//func RegisterEventHandlerType(eventHandlerType string, factory func(config map[interface{}]interface{}) EventHandlerFactory)  {
+//	registryLock.Lock()
+//	defer registryLock.Unlock()
+//	eventHandlerTypeFactories[eventHandlerType] = factory
+//}
 
 func RegisterEventHandler(id interface{}, config map[interface{}]interface{}) {
 	registryLock.Lock()
@@ -136,20 +159,20 @@ events:
 func WireEventHandlers() error {
 	registryLock.Lock()
 	defer registryLock.Unlock()
-
+	logger := pfxlog.Logger()
 	for _, eventHandlerConfig := range eventHandlerConfigs {
 		handler, err := eventHandlerConfig.createHandler()
 		if err != nil {
+			logger.Errorf("Unable to create event handler: %v", err)
 			return err
 		}
 		if err := eventHandlerConfig.processSubscriptions(handler); err != nil {
+			logger.Errorf("Unable to process subscription for event handler: %v", err)
 			return err
 		}
 	}
 
-	eventTypes = nil
-	eventHandlerTypeFactories = nil
-	eventHandlerConfigs = nil
-
 	return nil
 }
+
+

events/registrations.go
@@ -4,22 +4,23 @@ import (
 	"github.com/openziti/fabric/controller/network"
 	"github.com/openziti/fabric/trace"
 	"github.com/openziti/foundation/util/cowslice"
-	"github.com/pkg/errors"
-	"reflect"
 )
 
 func init() {
-	RegisterEventType("fabric.sessions", registerSessionEventHandler)
-	RegisterEventType("metrics", registerMetricsEventHandler)
 
-	RegisterEventType("fabric.traces", func(val interface{}, _ map[interface{}]interface{}) error {
-		handler, ok := val.(trace.EventHandler)
-		if !ok {
-			return errors.Errorf("type %v doesn't implement github.com/openziti/fabric/trace/EventHandler interface.", reflect.TypeOf(val))
-		}
-		AddTraceEventHandler(handler)
-		return nil
-	})
+	RegisterEventType("fabric.sessions", registerSessionEventHandler)
+	RegisterEventType("fabric.metrics", registerMetricsEventHandler)
+
+	// RegisterEventHandlerType("fabricHandler", registerFileLoggerEventHandlerType)
+
+	//RegisterEventType("fabric.traces", func(val interface{}, _ map[interface{}]interface{}) error {
+	//	handler, ok := val.(trace.EventHandler)
+	//	if !ok {
+	//		return errors.Errorf("type %v doesn't implement github.com/openziti/fabric/trace/EventHandler interface.", reflect.TypeOf(val))
+	//	}
+	//	AddTraceEventHandler(handler)
+	//	return nil
+	//})
 }
 
 func AddSessionEventHandler(handler network.SessionEventHandler) {