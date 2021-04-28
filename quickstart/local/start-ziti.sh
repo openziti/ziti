f1c80e08076abc3616dd96f4b313788562d1d277
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2021-04-28 00:31:45 -0400

    more cleanup - ensure some scripts are executable

--- quickstart/local/start-ziti.sh
+++
@@ -1,9 +0,0 @@
-ziti-router run "${ZITI_HOME}/${ZITI_ROUTER_BR_HOSTNAME}.yaml" > "${ZITI_HOME}/ziti-${ZITI_ROUTER_BR_HOSTNAME}.log" 2>&1 &
-sleep 1
-ziti-router run "${ZITI_HOME}/${ZITI_ROUTER_BLUE_HOSTNAME}.yaml" > "${ZITI_HOME}/ziti-${ZITI_ROUTER_BLUE_HOSTNAME}.log" 2>&1 &
-sleep 1
-ziti-router run "${ZITI_HOME}/${ZITI_ROUTER_RED_HOSTNAME}.yaml" > "${ZITI_HOME}/ziti-${ZITI_ROUTER_RED_HOSTNAME}.log" 2>&1 &
-sleep 1
-ziti-router run "${ZITI_HOME}/${ZITI_EDGE_ROUTER_HOSTNAME}.yaml" > "${ZITI_HOME}/ziti-${ZITI_EDGE_ROUTER_HOSTNAME}.log" 2>&1 &
-sleep 1
-ziti-router run "${ZITI_HOME}/${ZITI_EDGE_WSS_ROUTER_HOSTNAME}.yaml" > "${ZITI_HOME}/ziti-${ZITI_EDGE_WSS_ROUTER_HOSTNAME}.log" 2>&1 &
