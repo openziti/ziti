f9e85ca54ed9853d3e11b5ae799c91d2676446e6
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2021-05-14 10:25:09 -0400

    add a simple script that will simply push to docker (assuming you're logged into dockerhub)

---
+++ quickstart/docker/pushLatestDocker.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+ZITI_QUICKSTART_ROOT="$(realpath ${SCRIPT_DIR}/..)"
+ZITI_BIN_ROOT="${ZITI_QUICKSTART_ROOT}/docker"
+
+mkdir -p "${ZITI_BIN_ROOT}/ziti-bin"
+
+source "${ZITI_QUICKSTART_ROOT}/ziti-cli-functions.sh"
+
+ZITI_HOME="${ZITI_BIN_ROOT}"
+
+if [ -d "${ZITI_BIN_ROOT}/image/ziti.ignore" ]; then
+  rm -rf "${ZITI_BIN_ROOT}/image/ziti.ignore"
+fi
+
+getLatestZiti
+
+mv "${ZITI_BIN_DIR}" "${ZITI_BIN_ROOT}/image/ziti.ignore/"
+docker build "${ZITI_BIN_ROOT}/image" -t openziti/quickstart
+
+if [ -d "${ZITI_BIN_ROOT}/image/ziti.ignore" ]; then
+  rm -rf "${ZITI_BIN_ROOT}/image/ziti.ignore"
+fi
+
+docker run --rm -it openziti/quickstart /openziti/ziti-bin/ziti version
+if [ -f "${ZITI_BIN_ROOT}/ziti-*tar.gz" ]; then
+  rm "${ZITI_BIN_ROOT}/ziti-*tar.gz"
+fi
+if [ -d "${ZITI_BIN_ROOT}/ziti-bin" ]; then
+  rm -rf "${ZITI_BIN_ROOT}/ziti-bin"
+fi
+if [ -d "${ZITI_BIN_ROOT}/image/ziti.ignore" ]; then
+  rm -rf "${ZITI_BIN_ROOT}/image/ziti.ignore/"
+fi
+
+vers="$(echo "${ZITI_BINARIES_VERSION}" | cut -c 2-100)"
+docker tag openziti/quickstart "openziti/quickstart:${vers}"
+docker tag openziti/quickstart "openziti/quickstart:latest"
+docker push "openziti/quickstart:${vers}"
+docker push "openziti/quickstart:latest"
+
