2636536e7761c37abb8871948753c304ca45c5dc
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2022-04-19 17:05:20 -0400

    remove deprecated 'dotzeet'

--- quickstart/docker/image/ziti-cli-functions.sh
+++ quickstart/docker/image/ziti-cli-functions.sh
@@ -772,20 +772,9 @@ function createPki {
   pki_create_intermediate "${ZITI_SIGNING_ROOTCA_NAME}" "${ZITI_SPURIOUS_INTERMEDIATE}" 2
   pki_create_intermediate "${ZITI_SPURIOUS_INTERMEDIATE}" "${ZITI_SIGNING_INTERMEDIATE_NAME}" 1
 
-  if ! test -f "${ZITI_PKI}/${ZITI_CONTROLLER_INTERMEDIATE_NAME}/keys/${ZITI_NETWORK-}-dotzeet.key"; then
-    echo "Creating ziti-fabric client certificate for network: ${ZITI_NETWORK-}"
-    "${ZITI_BIN_DIR-}/ziti" pki create client --pki-root="${ZITI_PKI_OS_SPECIFIC}" --ca-name="${ZITI_CONTROLLER_INTERMEDIATE_NAME}" \
-          --client-file="${ZITI_NETWORK-}-dotzeet" \
-          --client-name "${ZITI_NETWORK-} Management"
-  else
-    echo "Creating ziti-fabric client certificate for network: ${ZITI_NETWORK-}"
-    echo "key exists"
-  fi
   echo " "
-
   pki_client_server "${ZITI_CONTROLLER_HOSTNAME}" "${ZITI_CONTROLLER_INTERMEDIATE_NAME}" "${ZITI_CONTROLLER_IP_OVERRIDE-}"
   pki_client_server "${ZITI_EDGE_CONTROLLER_HOSTNAME}" "${ZITI_EDGE_CONTROLLER_INTERMEDIATE_NAME}" "${ZITI_EDGE_CONTROLLER_IP_OVERRIDE-}"
-
 }
 
 
