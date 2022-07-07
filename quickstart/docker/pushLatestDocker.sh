5263431527f4766e9429ac890d68221217276986
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2022-07-07 09:28:02 -0400

    add echo's at the end

--- quickstart/docker/pushLatestDocker.sh
+++ quickstart/docker/pushLatestDocker.sh
@@ -37,8 +37,11 @@ if [ -d "${ZITI_BIN_ROOT}/image/ziti.ignore" ]; then
 fi
 
 vers="$(echo "${ZITI_BINARIES_VERSION}" | cut -c 2-100)"
+echo DOCKERTAG: docker tag openziti/quickstart "openziti/quickstart:${vers}"
 docker tag openziti/quickstart "openziti/quickstart:${vers}"
+echo DOCKERTAG: docker tag openziti/quickstart "openziti/quickstart:latest"
 docker tag openziti/quickstart "openziti/quickstart:latest"
+echo DOCKERPUSH: docker push "openziti/quickstart:${vers}"
 docker push "openziti/quickstart:${vers}"
+ecoh DOCKERPUSH: docker push "openziti/quickstart:latest"
 docker push "openziti/quickstart:latest"
-
