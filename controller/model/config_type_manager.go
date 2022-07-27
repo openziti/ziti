605935c224c61ccfd274266b7bfcaeee764d2d94
Paul Lorenz <paul.lorenz@netfoundry.io>
2022-07-27 12:11:50 -0400

    Update config types to use raft style commands. Fixes #1101

---
+++ controller/model/config_type_manager.go
@@ -0,0 +1,149 @@
+/*
+	Copyright NetFoundry Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	https://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+package model
+
+import (
+	"github.com/michaelquigley/pfxlog"
+	"github.com/openziti/edge/pb/edge_cmd_pb"
+	"github.com/openziti/fabric/controller/command"
+	"github.com/openziti/fabric/controller/fields"
+	"github.com/openziti/fabric/controller/models"
+	"github.com/openziti/fabric/controller/network"
+	"github.com/openziti/foundation/v2/stringz"
+	"go.etcd.io/bbolt"
+	"google.golang.org/protobuf/proto"
+)
+
+const (
+	ConfigTypeAll = "all"
+)
+
+func NewConfigTypeManager(env Env) *ConfigTypeManager {
+	manager := &ConfigTypeManager{
+		baseEntityManager: newBaseEntityManager(env, env.GetStores().ConfigType),
+	}
+	manager.impl = manager
+
+	network.RegisterManagerDecoder[*ConfigType](env.GetHostController().GetNetwork().Managers, manager)
+
+	return manager
+}
+
+type ConfigTypeManager struct {
+	baseEntityManager
+}
+
+func (self *ConfigTypeManager) newModelEntity() edgeEntity {
+	return &ConfigType{}
+}
+
+func (self *ConfigTypeManager) Create(entity *ConfigType) error {
+	return network.DispatchCreate[*ConfigType](self, entity)
+}
+
+func (self *ConfigTypeManager) ApplyCreate(cmd *command.CreateEntityCommand[*ConfigType]) error {
+	_, err := self.createEntity(cmd.Entity)
+	return err
+}
+
+func (self *ConfigTypeManager) Update(entity *ConfigType, checker fields.UpdatedFields) error {
+	return network.DispatchUpdate[*ConfigType](self, entity, checker)
+}
+
+func (self *ConfigTypeManager) ApplyUpdate(cmd *command.UpdateEntityCommand[*ConfigType]) error {
+	return self.updateEntity(cmd.Entity, cmd.UpdatedFields)
+}
+
+func (self *ConfigTypeManager) Read(id string) (*ConfigType, error) {
+	modelEntity := &ConfigType{}
+	if err := self.readEntity(id, modelEntity); err != nil {
+		return nil, err
+	}
+	return modelEntity, nil
+}
+
+func (self *ConfigTypeManager) readInTx(tx *bbolt.Tx, id string) (*ConfigType, error) {
+	modelEntity := &ConfigType{}
+	if err := self.readEntityInTx(tx, id, modelEntity); err != nil {
+		return nil, err
+	}
+	return modelEntity, nil
+}
+
+func (self *ConfigTypeManager) ReadByName(name string) (*ConfigType, error) {
+	modelEntity := &ConfigType{}
+	nameIndex := self.env.GetStores().ConfigType.GetNameIndex()
+	if err := self.readEntityWithIndex("name", []byte(name), nameIndex, modelEntity); err != nil {
+		return nil, err
+	}
+	return modelEntity, nil
+}
+
+func (self *ConfigTypeManager) MapConfigTypeNamesToIds(values []string, identityId string) map[string]struct{} {
+	var result []string
+	if stringz.Contains(values, "all") {
+		result = []string{"all"}
+	} else {
+		for _, val := range values {
+			if configType, _ := self.Read(val); configType != nil {
+				result = append(result, val)
+			} else if configType, _ := self.ReadByName(val); configType != nil {
+				result = append(result, configType.Id)
+			} else {
+				pfxlog.Logger().Debugf("user %v submitted %v as a config type of interest, but no matching records found", identityId, val)
+			}
+		}
+	}
+	return stringz.SliceToSet(result)
+}
+
+func (self *ConfigTypeManager) Marshall(entity *ConfigType) ([]byte, error) {
+	tags, err := edge_cmd_pb.EncodeTags(entity.Tags)
+	if err != nil {
+		return nil, err
+	}
+
+	schema, err := edge_cmd_pb.EncodeJson(entity.Schema)
+	if err != nil {
+		return nil, err
+	}
+
+	msg := &edge_cmd_pb.ConfigType{
+		Id:     entity.Id,
+		Name:   entity.Name,
+		Schema: schema,
+		Tags:   tags,
+	}
+
+	return proto.Marshal(msg)
+}
+
+func (self *ConfigTypeManager) Unmarshall(bytes []byte) (*ConfigType, error) {
+	msg := &edge_cmd_pb.ConfigType{}
+	if err := proto.Unmarshal(bytes, msg); err != nil {
+		return nil, err
+	}
+
+	return &ConfigType{
+		BaseEntity: models.BaseEntity{
+			Id:   msg.Id,
+			Tags: edge_cmd_pb.DecodeTags(msg.Tags),
+		},
+		Name:   msg.Name,
+		Schema: edge_cmd_pb.DecodeJson(msg.Schema),
+	}, nil
+}
