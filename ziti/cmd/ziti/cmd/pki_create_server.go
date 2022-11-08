b3dadf57a6da0da129c383c386e7eda3cfb7f482
Paul Lorenz <paul.lorenz@netfoundry.io>
2022-11-08 17:12:38 -0500

    Update deps and add spiffe-id support

--- ziti/cmd/ziti/cmd/pki_create_server.go
+++ ziti/cmd/ziti/cmd/pki_create_server.go
@@ -17,15 +17,15 @@
 package cmd
 
 import (
-	"fmt"
-	"github.com/spf13/cobra"
-	"io"
-
 	cmdhelper "github.com/openziti/ziti/ziti/cmd/ziti/cmd/helpers"
 	"github.com/openziti/ziti/ziti/cmd/ziti/internal/log"
 	"github.com/openziti/ziti/ziti/pki/certificate"
 	"github.com/openziti/ziti/ziti/pki/pki"
 	"github.com/openziti/ziti/ziti/pki/store"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"io"
+	"net/url"
 )
 
 // PKICreateServerOptions the options for the create spring command
@@ -73,19 +73,19 @@ func (o *PKICreateServerOptions) addPKICreateServerFlags(cmd *cobra.Command) {
 	cmd.Flags().IntVarP(&o.Flags.CAExpire, "expire-limit", "", 365, "Expiration limit in days")
 	cmd.Flags().IntVarP(&o.Flags.CAMaxpath, "max-path-len", "", -1, "Intermediate maximum path length")
 	cmd.Flags().IntVarP(&o.Flags.CAPrivateKeySize, "private-key-size", "", 4096, "Size of the private key")
+	cmd.Flags().StringVar(&o.Flags.SpiffeID, "spiffe-id", "", "Optionally provide the path portion of a SPIFFE id. The trust domain will be taken from the signing certificate.")
 }
 
 // Run implements this command
 func (o *PKICreateServerOptions) Run() error {
-
 	IPs, DNSNames, err := o.ObtainIPsAndDNSNames()
 	if err != nil {
-		return fmt.Errorf("%s", err)
+		return err
 	}
 
 	pkiroot, err := o.ObtainPKIRoot()
 	if err != nil {
-		return fmt.Errorf("%s", err)
+		return err
 	}
 
 	o.Flags.PKI = &pki.ZitiPKI{Store: &store.Local{}}
@@ -96,7 +96,7 @@ func (o *PKICreateServerOptions) Run() error {
 
 	serverCertFile, err := o.ObtainServerCertFile()
 	if err != nil {
-		return fmt.Errorf("%s", err)
+		return err
 	}
 
 	filename := o.ObtainFileName(serverCertFile, commonName)
@@ -110,17 +110,37 @@ func (o *PKICreateServerOptions) Run() error {
 
 	caname, err := o.ObtainCAName(pkiroot)
 	if err != nil {
-		return fmt.Errorf("%s", err)
+		return err
 	}
 
 	keyFile, err := o.ObtainKeyFile(false)
 	if err != nil {
-		return fmt.Errorf("%s", err)
+		return err
 	}
 
 	signer, err = o.Flags.PKI.GetCA(caname)
 	if err != nil {
-		return fmt.Errorf("Cannot locate signer: %v", err)
+		return errors.Wrap(err, "cannot locate signer")
+	}
+
+	if o.Flags.SpiffeID != "" {
+		var trustDomain *url.URL
+		for _, uri := range signer.Cert.URIs {
+			if uri.Scheme == "spiffe" {
+				if trustDomain != nil {
+					return errors.New("signing cert contained multiple spiffe ids, which is not allowed")
+				}
+				trustDomain = uri
+			}
+		}
+
+		if trustDomain == nil {
+			return errors.New("signing cert doesn't have a spiffe id. unknown trust domain")
+		}
+
+		spiffId := *trustDomain
+		spiffId.Path = o.Flags.SpiffeID
+		template.URIs = append(template.URIs, &spiffId)
 	}
 
 	req := &pki.Request{
@@ -132,12 +152,12 @@ func (o *PKICreateServerOptions) Run() error {
 	}
 
 	if err := o.Flags.PKI.Sign(signer, req); err != nil {
-		return fmt.Errorf("Cannot Sign: %v", err)
+		return errors.Wrap(err, "cannot sign")
 	}
 
 	// Concat the newly-created server cert with the intermediate cert to create a server.chain.pem file
 	if err := o.Flags.PKI.Chain(signer, req); err != nil {
-		return fmt.Errorf("Cannot Sign: %v", err)
+		return errors.Wrap(err, "cannot sign")
 	}
 
 	log.Infoln("Success")
