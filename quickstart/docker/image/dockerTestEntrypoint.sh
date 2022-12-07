24a855c292bd7df4b079a574c6a4617bba322b5c
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2022-12-06 19:46:42 -0500

    unbotch the merge conflict resolution

--- quickstart/docker/image/dockerTestEntrypoint.sh
+++ quickstart/docker/image/dockerTestEntrypoint.sh
@@ -8,4 +8,4 @@ touch runonce
 source "${ZITI_SCRIPTS}/ziti-cli-functions.sh"
 # Set the default password to be a specific value, if not set the password will be a random string and tests will fail
 export ZITI_PWD=admin
-expressInstall "${ZITI_NETWORK}"
+expressInstall "localhost"
