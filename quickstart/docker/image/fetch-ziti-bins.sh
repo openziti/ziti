fdf0936ec9b6096733d012a904bf62f90abb2e9e
Shawn Carey <shawn.carey@netfoundry.io>
2023-01-13 16:20:53 -0500

    move binaries into place

--- quickstart/docker/image/fetch-ziti-bins.sh
+++ quickstart/docker/image/fetch-ziti-bins.sh
@@ -7,8 +7,7 @@ dest="${1}"
 if [ -d /docker.build.context/ziti-bin -a ]; then
   mv /docker.build.context/ziti-bin/ "${dest}"
 else
-   ZITI_BIN_ROOT="${dest}"
-   ZITI_BIN_DIR="${ZITI_BIN_ROOT}"
    source /docker.build.context/ziti-cli-functions.sh
    getZiti
+   mv "${ZITI_BIN_DIR}" "${dest}"
 fi
