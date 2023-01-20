1cb0cf0bd69936e621b461a4b1f998daf61a56c5
Shawn Carey <shawn.carey@netfoundry.io>
2023-01-20 09:11:29 -0500

    Use docker buildx to build/push Quickstart image (#949)
    use buildx to build quickstart image

--- quickstart/docker/pushLatestDocker.sh
+++ quickstart/docker/pushLatestDocker.sh
@@ -3,32 +3,20 @@ set -eo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-ZITI_QUICKSTART_ROOT="$(realpath ${SCRIPT_DIR}/..)"
-ZITI_BIN_ROOT="${ZITI_QUICKSTART_ROOT}/docker"
+if [ -z "${ZITI_VERSION}" ]; then
+  ZITI_QUICKSTART_ROOT="$(realpath ${SCRIPT_DIR}/..)"
+  v=$(source "${ZITI_QUICKSTART_ROOT}/ziti-cli-functions.sh"; getLatestZitiVersion > /dev/null 2>&1; echo ${ZITI_BINARIES_VERSION})
+  ZITI_VERSION=$(echo "${v}" | sed -e 's/^v//')
+fi
 
-for arch in amd64 arm64; do
-  (export ZITI_OSTYPE=linux; export ZITI_ARCH="${arch}"
-   source "${ZITI_QUICKSTART_ROOT}/ziti-cli-functions.sh"
-   if [ -d "${SCRIPT_DIR}/image/ziti.ignore" ]; then
-     rm -rf "${SCRIPT_DIR}/image/ziti.ignore"
-   fi
+if [ -z "${ZITI_VERSION}" ]; then
+  echo "ZITI_VERSION was not set and auto-detection failed."
+  exit 1
+fi
 
-   getZiti
-
-   mv "${ZITI_BIN_DIR}" "${SCRIPT_DIR}/image/ziti.ignore/"
-   docker build --platform linux/${arch} "${SCRIPT_DIR}/image" -t openziti/quickstart
-
-   if [ -d "${SCRIPT_DIR}/image/ziti.ignore" ]; then
-     rm -rf "${SCRIPT_DIR}/image/ziti.ignore"
-   fi
-
-   vers="$(echo "${ZITI_BINARIES_VERSION}" | cut -c 2-100)"
-   echo DOCKERTAG: docker tag openziti/quickstart "openziti/quickstart:${vers}"
-   docker tag openziti/quickstart "openziti/quickstart:${vers}"
-   echo DOCKERTAG: docker tag openziti/quickstart "openziti/quickstart:latest"
-   docker tag openziti/quickstart "openziti/quickstart:latest"
-   echo DOCKERPUSH: docker push "openziti/quickstart:${vers}"
-   docker push "openziti/quickstart:${vers}"
-   echo DOCKERPUSH: docker push "openziti/quickstart:latest"
-   docker push "openziti/quickstart:latest")
-done
+docker buildx create --use --name=ziti-builder
+docker buildx build --platform linux/amd64,linux/arm64 "${SCRIPT_DIR}/image" \
+  --build-arg ZITI_VERSION_OVERRIDE="${ZITI_VERSION}" \
+  --tag "openziti/quickstart:${ZITI_VERSION}" \
+  --tag "openziti/quickstart:latest" \
+  --push
