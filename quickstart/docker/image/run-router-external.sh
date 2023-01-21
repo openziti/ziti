6487dc0aae15aa165dacaa151517e53c16f03135
Kelvin Smith <kelvin@thesmithcave.nz>
2023-01-21 21:46:32 +0000

    Updated for definable hostname in controller

--- quickstart/docker/image/run-router-external.sh
+++ quickstart/docker/image/run-router-external.sh
@@ -7,24 +7,30 @@ sleep 5
 . "${ZITI_SCRIPTS}/ziti-cli-functions.sh"
 
 # If we have not defined these, then no point going further - so dont
-if [[ "${ZITI_EDGE_ROUTER_RAWNAME_OVERRIDE}" == "" ]]; then echo "ERROR: Missing ZITI_EDGE_ROUTER_RAWNAME_OVERRIDE definition" >&2; exit; fi
+if [[ "${ZITI_EDGE_ROUTER_HOSTNAME}" == "" ]]; then echo "ERROR: Missing ZITI_EDGE_ROUTER_HOSTNAME definition" >&2; exit; fi
 export ZITI_EDGE_ROUTER_RAWNAME=${ZITI_EDGE_ROUTER_RAWNAME_OVERRIDE}
 
-echo "ZITI_EDGE_ROUTER_RAWNAME = ${ZITI_EDGE_ROUTER_RAWNAME}"
+echo "ZITI_EDGE_ROUTER_HOSTNAME = ${ZITI_EDGE_ROUTER_HOSTNAME}"
 
 
 # If we dont have a key file, then we assume we haven't enrolled, so lets do that
-if [ ! -f ${ZITI_EDGE_ROUTER_RAWNAME}.key ]; then
+if [ ! -f ${ZITI_EDGE_ROUTER_HOSTNAME}.jwt ]; then
 
   # May have been a bad attempt before, so lets clean up
-  rm -f ${ZITI_HOME}/${ZITI_EDGE_ROUTER_RAWNAME}*
-  
+  rm -f ${ZITI_HOME}/${ZITI_EDGE_ROUTER_HOSTNAME}*
+
+
   # If we have override variables then override the base variable
   # This is required as the .bashrc loads the env file which clobbers passed in varibles
+  if [[ "${ZITI_EDGE_ROUTER_RAWNAME_OVERRIDE}" == "" ]]; then echo "ERROR: Missing ZITI_EDGE_ROUTER_RAWNAME_OVERRIDE definition" >&2; exit; fi
+    # May have been a bad attempt before, so lets clean up
+  rm -f ${ZITI_HOME}/${ZITI_EDGE_ROUTER_RAWNAME}*
+
   if [[ ${ZITI_CTRL_ADVERTISED_ADDRESS_OVERRIDE} != "" ]]; then export ZITI_CTRL_ADVERTISED_ADDRESS=${ZITI_CTRL_ADVERTISED_ADDRESS_OVERRIDE}; fi
   if [[ ${ZITI_USER_OVERRIDE} != "" ]]; then export ZITI_USER=${ZITI_USER_OVERRIDE}; fi
   if [[ ${ZITI_PWD_OVERRIDE} != "" ]]; then export ZITI_PWD=${ZITI_PWD_OVERRIDE}; fi
   if [[ ${ZITI_EDGE_CONTROLLER_PORT_OVERRIDE} != "" ]]; then export ZITI_EDGE_CONTROLLER_PORT=${ZITI_EDGE_CONTROLLER_PORT_OVERRIDE}; fi
+  if [[ ${ZITI_EDGE_ROUTER_HOSTNAME} == "" ]]; then export ZITI_EDGE_ROUTER_HOSTNAME=${ZITI_EDGE_ROUTER_RAWNAME}; fi
 
   # First off - lets make sure we have what we need
   if [[ ${ZITI_USER} == "" ]] || [[ ${ZITI_PWD} == "" ]]; then echo "ERROR:  Need ZITI_USER and ZITI_PWD defined" >&2; exit; fi
@@ -33,7 +39,7 @@ if [ ! -f ${ZITI_EDGE_ROUTER_RAWNAME}.key ]; then
   if [[ "${ZITI_EDGE_ROUTER_PORT-}" == "" ]]; then export ZITI_EDGE_ROUTER_PORT="3022"; fi
   if [[ "${ZITI_EDGE_ROUTER_ROLES}" == "" ]]; then export ZITI_EDGE_ROUTER_ROLES="${ZITI_EDGE_ROUTER_HOSTNAME}"; fi
 
-
+  echo "ZITI_EDGE_ROUTER_RAWNAME = ${ZITI_EDGE_ROUTER_RAWNAME}"
   echo "ZITI_EDGE_ROUTER_ROLES = ${ZITI_EDGE_ROUTER_ROLES}"
   echo "ZITI_EDGE_ROUTER_PORT = ${ZITI_EDGE_ROUTER_PORT}"
   echo "ZITI_CTRL_ADVERTISED_ADDRESS = ${ZITI_CTRL_ADVERTISED_ADDRESS}"
@@ -61,20 +67,21 @@ if [ ! -f ${ZITI_EDGE_ROUTER_RAWNAME}.key ]; then
     createPrivateRouterConfig "${ZITI_EDGE_ROUTER_RAWNAME}"
   fi
 
+  mv ${ZITI_HOME}/${ZITI_EDGE_ROUTER_RAWNAME}.yaml ${ZITI_HOME}/${ZITI_EDGE_ROUTER_HOSTNAME}.yaml
 
   echo "----------  Creating edge-router ${ZITI_EDGE_ROUTER_RAWNAME}...."
-  found=$(${ZITI_BIN_DIR}/ziti edge list edge-routers 'name = "'"${ZITI_EDGE_ROUTER_RAWNAME}"'"' | grep -c "${ZITI_EDGE_ROUTER_RAWNAME}")
+  found=$(${ZITI_BIN_DIR}/ziti edge list edge-routers 'name = "'"${ZITI_EDGE_ROUTER_HOSTNAME}"'"' | grep -c "${ZITI_EDGE_ROUTER_HOSTNAME}")
   if [[ found -gt 0 ]]; then
-    echo "----------  Found existing edge-router ${ZITI_EDGE_ROUTER_RAWNAME}...."
+    echo "----------  Found existing edge-router ${ZITI_EDGE_ROUTER_HOSTNAME}...."
   else
-    "${ZITI_BIN_DIR}/ziti" edge create edge-router "${ZITI_EDGE_ROUTER_RAWNAME}" -o "${ZITI_HOME}/${ZITI_EDGE_ROUTER_RAWNAME}.jwt" -t -a "${ZITI_EDGE_ROUTER_ROLES}"
+    "${ZITI_BIN_DIR}/ziti" edge create edge-router "${ZITI_EDGE_ROUTER_HOSTNAME}" -o "${ZITI_HOME}/${ZITI_EDGE_ROUTER_HOSTNAME}.jwt" -t -a "${ZITI_EDGE_ROUTER_ROLES}"
     sleep 1
-    echo "---------- Enrolling edge-router ${ZITI_EDGE_ROUTER_RAWNAME}...."
-  "${ZITI_BIN_DIR}/ziti-router" enroll "${ZITI_HOME}/${ZITI_EDGE_ROUTER_RAWNAME}.yaml" --jwt "${ZITI_HOME}/${ZITI_EDGE_ROUTER_RAWNAME}.jwt"
+    echo "---------- Enrolling edge-router ${ZITI_EDGE_ROUTER_HOSTNAME}...."
+  "${ZITI_BIN_DIR}/ziti-router" enroll "${ZITI_HOME}/${ZITI_EDGE_ROUTER_HOSTNAME}.yaml" --jwt "${ZITI_HOME}/${ZITI_EDGE_ROUTER_HOSTNAME}.jwt"
     echo ""
   fi
 fi
 
 # Run the router
-"${ZITI_BIN_DIR}/ziti-router" run "${ZITI_HOME}/${ZITI_EDGE_ROUTER_RAWNAME}.yaml" > "${ZITI_HOME}/ziti-${ZITI_EDGE_ROUTER_RAWNAME}.log"
+"${ZITI_BIN_DIR}/ziti-router" run "${ZITI_HOME}/${ZITI_EDGE_ROUTER_HOSTNAME}.yaml" > "${ZITI_HOME}/ziti-${ZITI_EDGE_ROUTER_HOSTNAME}.log"
 
