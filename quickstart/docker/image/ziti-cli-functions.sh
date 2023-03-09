f1c2223b3f427eb300c39d3f1a93eae35b1b89dc
Dhanush P Sukruth <dpshetty1999@gmail.com>
2023-03-09 23:55:01 +0530

    Revert "Fix #920"

--- quickstart/docker/image/ziti-cli-functions.sh
+++ quickstart/docker/image/ziti-cli-functions.sh
@@ -464,7 +464,9 @@ function ziti_expressConfiguration {
         fi
       fi
 
-      
+      # Silently clear ziti variables
+      unsetZitiEnv "-s"
+
       if [[ "${specifiedVersion}" != "" ]]; then
         export ZITI_VERSION_OVERRIDE="${specifiedVersion}"
       fi
@@ -472,10 +474,6 @@ function ziti_expressConfiguration {
       # Stop any devices currently running to avoid port collisions
       stopRouter
       stopController
-      
-      # Silently clear ziti variables
-      unsetZitiEnv "-s"
-
     else
       echo -e "$(RED "  --- Exiting express install ---")"
       return 1
