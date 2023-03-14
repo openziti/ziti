cce22ebb3642d31f696aec4dab7ceb1dcaec36cb
Paul Lorenz <paul.lorenz@netfoundry.io>
2023-03-14 09:56:08 -0400

    Remove adaptive buffer sizing

--- router/xgress_common/connection.go
+++ router/xgress_common/connection.go
@@ -43,12 +43,7 @@ const (
 )
 
 const (
-	DefaultMinReadBufferSize      = 8 * 1024
-	DefaultMaxReadBufferSize      = 62 * 1024
-	DefaultStartingReadBufferSize = 32 * 1024
-
-	DefaultReadBufferAdjustmentSize  = 2 * 1024
-	DefaultReadBufferUpperSlopFactor = 4 * 1024
+	DefaultBufferSize = 10 * 1024
 )
 
 type outOfBand struct {
@@ -66,9 +61,7 @@ type XgressConn struct {
 
 	writeDone  chan struct{}
 	flags      concurrenz.AtomicBitSet
-	minBuffer  int
-	maxBuffer  int
-	currBuffer int
+	bufferSize int
 }
 
 func NewXgressConn(conn net.Conn, halfClose bool, isTransport bool) *XgressConn {
@@ -76,15 +69,11 @@ func NewXgressConn(conn net.Conn, halfClose bool, isTransport bool) *XgressConn 
 		Conn:        conn,
 		outOfBandTx: make(chan *outOfBand, 1),
 		writeDone:   make(chan struct{}),
-		minBuffer:   DefaultMinReadBufferSize,
-		maxBuffer:   DefaultMaxReadBufferSize,
-		currBuffer:  DefaultStartingReadBufferSize,
+		bufferSize:  DefaultBufferSize,
 	}
 
 	if _, isUdpConn := conn.(*net.UDPConn); isUdpConn {
-		result.minBuffer = info.MaxUdpPacketSize
-		result.maxBuffer = info.MaxUdpPacketSize
-		result.currBuffer = info.MaxUdpPacketSize
+		result.bufferSize = info.MaxUdpPacketSize
 	}
 
 	result.flags.Set(halfCloseFlag, halfClose)
@@ -194,9 +183,8 @@ func (self *XgressConn) ReadPayload() ([]byte, map[uint8][]byte, error) {
 		}
 	}
 
-	buffer := make([]byte, self.currBuffer)
+	buffer := make([]byte, self.bufferSize)
 	n, err := self.Conn.Read(buffer)
-	self.calculateNextBufferSize(n)
 	buffer = buffer[:n]
 
 	if self.sender != nil && n > 0 {
@@ -218,23 +206,6 @@ func (self *XgressConn) ReadPayload() ([]byte, map[uint8][]byte, error) {
 	}
 
 	return buffer, nil, err
-}
-
-func (self *XgressConn) calculateNextBufferSize(bytesRead int) {
-	// if we could have theoretically read mode, see if we can increase buffer size
-	if self.currBuffer == bytesRead {
-		if self.currBuffer >= self.maxBuffer {
-			return
-		}
-		self.currBuffer += DefaultReadBufferAdjustmentSize // bump buffer size by 2k
-		return
-	}
-
-	// If we're already at min buffer size, or if the read bytes were with the slop factor, don't try to optimize further
-	if self.currBuffer <= self.minBuffer || (self.currBuffer-DefaultReadBufferUpperSlopFactor) < bytesRead {
-		return
-	}
-	self.currBuffer -= DefaultReadBufferAdjustmentSize
 }
 
 func (self *XgressConn) WritePayload(p []byte, headers map[uint8][]byte) (int, error) {
