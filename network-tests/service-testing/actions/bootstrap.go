1376f7a016fe5815dfd0f23374fde167fd4a2ac6
Pete DiBona <pete.dibona@netfoundry.io>
2023-03-28 12:33:41 -0500

    Cleaned up code a bit and added appropriate comments.

--- network-tests/service-testing/actions/bootstrap.go
+++ network-tests/service-testing/actions/bootstrap.go
@@ -23,6 +23,7 @@ func NewBootstrapAction() model.ActionBinder {
 func (a *bootstrapAction) bind(m *model.Model) model.Action {
 	workflow := actions.Workflow()
 
+	// Setup Ziti Controller
 	workflow.AddAction(host.GroupExec("*", 25, "rm -f logs/*"))
 	workflow.AddAction(component.Stop("#ctrl"))
 	workflow.AddAction(semaphore.Sleep(2 * time.Second))
@@ -31,19 +32,23 @@ func (a *bootstrapAction) bind(m *model.Model) model.Action {
 	workflow.AddAction(component.Start("#ctrl"))
 	workflow.AddAction(semaphore.Sleep(10 * time.Second))
 
+	// Login to Ziti Controller
 	workflow.AddAction(edge.Login("#ctrl"))
 	workflow.AddAction(semaphore.Sleep(2 * time.Second))
 
+	// Setup Ziti Routers
 	workflow.AddAction(component.StopInParallel(models.EdgeRouterTag, 25))
 	workflow.AddAction(semaphore.Sleep(2 * time.Second))
 	workflow.AddAction(edge.InitEdgeRouters(models.EdgeRouterTag, 2))
 	workflow.AddAction(semaphore.Sleep(2 * time.Second))
 	workflow.AddAction(component.StartInParallel(models.EdgeRouterTag, 25))
 	workflow.AddAction(semaphore.Sleep(10 * time.Second))
+
+	// Init Identities
 	workflow.AddAction(edge.InitIdentities(models.SdkAppTag, 2))
 	workflow.AddAction(semaphore.Sleep(2 * time.Second))
 
-	//workflow.AddAction(zitilib_actions.Edge("create", "service", "echo"))
+	// Create Configs
 	workflow.AddAction(zitilib_actions.Edge("create", "config", "iperf-server", "host.v1", `		 
 					{
 							"address" : "localhost",
@@ -60,8 +65,11 @@ func (a *bootstrapAction) bind(m *model.Model) model.Action {
 			"protocols": ["tcp"]
 		}`))
 	workflow.AddAction(semaphore.Sleep(2 * time.Second))
+
+	// Create Service
 	workflow.AddAction(zitilib_actions.Edge("create", "service", "iperf", "-c", "iperf-server,iperf-intercept"))
 
+	// Enroll Policies
 	workflow.AddAction(zitilib_actions.Edge("create", "service-policy", "iperf-server", "Bind", "--service-roles",
 		"@iperf", "--identity-roles", "#iperf-server"))
 	workflow.AddAction(zitilib_actions.Edge("create", "service-policy", "iperf-client", "Dial", "--service-roles",
@@ -72,7 +80,10 @@ func (a *bootstrapAction) bind(m *model.Model) model.Action {
 		"#iperf-server", "--identity-roles", "#iperf-server"))
 	workflow.AddAction(zitilib_actions.Edge("create", "service-edge-router-policy", "iperf", "--semantic", "AnyOf",
 		"--service-roles", "@iperf", "--edge-router-roles", "#all"))
+
+	// Start Beats Services
 	workflow.AddAction(host.GroupExec("*", 25, "sudo service filebeat stop; sleep 5; sudo service filebeat start; sleep 5; sudo service filebeat status"))
 	workflow.AddAction(host.GroupExec("*", 25, "sudo service metricbeat stop; sleep 5; sudo service metricbeat start; sleep 5; sudo service metricbeat status"))
+
 	return workflow
 }
