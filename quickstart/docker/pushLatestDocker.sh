7066c3aacc65b260897fd8b3d8e80eabbf6ff907
gberl002 <geoff.berl@netfoundry.io>
2023-06-21 20:50:11 -0400

    Update path to ziti cli functions for docker pusher
    Signed-off-by: gberl002 <geoff.berl@netfoundry.io>

--- quickstart/docker/pushLatestDocker.sh
+++ quickstart/docker/pushLatestDocker.sh
@@ -4,8 +4,8 @@ set -eo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 if [ -z "${ZITI_VERSION}" ]; then
-  ZITI_QUICKSTART_ROOT="$(realpath ${SCRIPT_DIR}/..)"
-  v=$(source "${ZITI_QUICKSTART_ROOT}/ziti-cli-functions.sh"; getLatestZitiVersion > /dev/null 2>&1; echo ${ZITI_BINARIES_VERSION})
+  DOCKER_IMAGE_ROOT="$(realpath ${SCRIPT_DIR}/image)"
+  v=$(source "${DOCKER_IMAGE_ROOT}/ziti-cli-functions.sh"; getLatestZitiVersion > /dev/null 2>&1; echo ${ZITI_BINARIES_VERSION})
   ZITI_VERSION=$(echo "${v}" | sed -e 's/^v//')
 fi
 
