34ff8ab6a6a8e268a4ad280f2cd63cd2156a80d6
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2023-06-30 12:23:42 -0400

    add warning to let people know they aren't overriding the ziti cli with a built version

--- quickstart/docker/createLocalImage.sh
+++ quickstart/docker/createLocalImage.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 ###
 ### This script will recreate the *LATEST* quickstart image locally from *PUBLISHED* binaries
 ### use this script when you're updating/changing the scripts, file locations inside the container
 ### to quickly/easily recreate the image for local dev use that CI will publish
 ###
 echo "CREATING latest quickstart containers LOCALLY"
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+if [ -d != "${SCRIPT_DIR}/ziti-bin" ]; then
+  echo ""
+  echo "WARNING: no image/ziti-bin directory found. Using latest published relese"
+fi
+
 $SCRIPT_DIR/pushLatestDocker.sh local
