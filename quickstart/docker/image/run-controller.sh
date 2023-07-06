45c40c70c6bcb9da2cae851ed997adfe76d5a1b3
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2023-07-06 08:19:46 -0400

    don't recreate everything everytime

--- quickstart/docker/image/run-controller.sh
+++ quickstart/docker/image/run-controller.sh
@@ -9,25 +9,36 @@ if [[ "${ZITI_CTRL_NAME-}" == "" ]]; then export ZITI_CTRL_NAME="${ZITI_NETWORK}
 if [ ! -f "${ZITI_HOME}/access-control.init" ]; then
   setupEnvironment
   persistEnvironmentValues
-fi
-. ${ZITI_HOME}/ziti.env
+  # don't move the sourcing of the file. yes it's duplicated but it needs to be here
+  . ${ZITI_HOME}/ziti.env
 
-# create pki
-createPki
-if [ ! -f "${ziti_controller_cfg}" ]; then
-  echo " "
-  echo "${ziti_controller_cfg} doesn't exist. Generating config file"
-  echo " "
-  # generates the config file for the controller
-  createControllerConfig
+  if [ ! -f "${ZITI_HOME}/access-control.init" ]; then
+    setupEnvironment
+    persistEnvironmentValues
+  else
+    echo "system has been initialized already. just starting the process"
+  fi
+  # create pki
+  createPki
+  if [ ! -f "${ziti_controller_cfg}" ]; then
+    echo " "
+    echo "${ziti_controller_cfg} doesn't exist. Generating config file"
+    echo " "
+    # generates the config file for the controller
+    createControllerConfig
+  else
+    echo " "
+    echo "${ziti_controller_cfg} exists. Not overwriting"
+    echo " "
+  fi
+
+  # initialize the database with the admin user:
+  "${ZITI_BIN_DIR}/ziti" controller edge init "${ZITI_HOME}/${ZITI_CTRL_NAME}.yaml" -u "${ZITI_USER}" -p "${ZITI_PWD}"
 else
-  echo " "
-  echo "${ziti_controller_cfg} exists. Not overwriting"
-  echo " "
+  echo "system has been initialized already. just starting the process"
+  # don't move the sourcing of the file. yes it's duplicated but it needs to be here
+  . ${ZITI_HOME}/ziti.env
 fi
-
-# initialize the database with the admin user:
-"${ZITI_BIN_DIR}/ziti" controller edge init "${ZITI_HOME}/${ZITI_CTRL_NAME}.yaml" -u "${ZITI_USER}" -p "${ZITI_PWD}"
 
 echo "controller initialized. unsetting ZITI_USER/ZITI_PWD from env"
 unset ZITI_USER
