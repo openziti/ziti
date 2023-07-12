55c2a0b100fede9cd6f474adf11eefe2bdc87ee7
Pete DiBona <pete.dibona@netfoundry.io>
2023-07-12 17:06:00 -0500

    Altered Bootstrap to work with new functions
    Signed-off-by: Pete DiBona <pete.dibona@netfoundry.io>

--- zititest/models/db-creation/actions/bootstrap.go
+++ zititest/models/db-creation/actions/bootstrap.go
@@ -1,14 +1,31 @@
+/*
+	(c) Copyright NetFoundry Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	https://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
 package actions
 
 import (
-	zitilib_actions "github.com/openziti/ziti/zititest/zitilab/actions"
+	"github.com/openziti/fablab/kernel/lib/actions/semaphore"
+	"github.com/openziti/ziti/zititest/zitilab"
 	"time"
 
 	"github.com/openziti/fablab/kernel/lib/actions"
 	"github.com/openziti/fablab/kernel/lib/actions/component"
 	"github.com/openziti/fablab/kernel/lib/actions/host"
-	"github.com/openziti/fablab/kernel/lib/actions/semaphore"
 	"github.com/openziti/fablab/kernel/model"
+	zitilib_actions "github.com/openziti/ziti/zititest/zitilab/actions"
 	"github.com/openziti/ziti/zititest/zitilab/actions/edge"
 	"github.com/openziti/ziti/zititest/zitilab/models"
 )
@@ -23,15 +40,11 @@ func NewBootstrapAction() model.ActionBinder {
 func (a *bootstrapAction) bind(m *model.Model) model.Action {
 	workflow := actions.Workflow()
 
-	// Setup Ziti Controller
-	workflow.AddAction(component.Stop(".ctrl"))
 	workflow.AddAction(host.GroupExec("*", 25, "rm -f logs/*"))
-	workflow.AddAction(host.GroupExec(".ctrl", 5, "rf -rf ./fablab/ctrldata"))
-	workflow.AddAction(component.Start(".ctrl"))
-	workflow.AddAction(semaphore.Sleep(2 * time.Second))
-	//Comment out the below line if you are inserting your own DB.
-	workflow.AddAction(edge.InitController("#ctrl"))
-	workflow.AddAction(semaphore.Sleep(2 * time.Second))
+	workflow.AddAction(component.Stop("#ctrl"))
+	workflow.AddAction(component.Exec("#ctrl", zitilab.ControllerActionInitStandalone))
+	workflow.AddAction(component.Start("#ctrl"))
+	workflow.AddAction(edge.ControllerAvailable("#ctrl", 30*time.Second))
 
 	// Login to Ziti Controller
 	workflow.AddAction(edge.Login("#ctrl"))
@@ -39,7 +52,6 @@ func (a *bootstrapAction) bind(m *model.Model) model.Action {
 
 	// Setup Ziti Routers
 	workflow.AddAction(component.StopInParallel(models.EdgeRouterTag, 25))
-	workflow.AddAction(semaphore.Sleep(2 * time.Second))
 	workflow.AddAction(edge.InitEdgeRouters(models.EdgeRouterTag, 2))
 	workflow.AddAction(semaphore.Sleep(2 * time.Second))
 
