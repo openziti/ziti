22b54e8558ecb5ef16d9b7db9dda72b49a8832d9
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2023-11-22 12:12:38 -0500

    promote log from debug to info

--- router/internal/edgerouter/config.go
+++ router/internal/edgerouter/config.go
@@ -310,7 +310,7 @@ func parseEdgeListenerOptions(index int, address string, edgeListenerMap map[int
 			}
 
 			if advertisePort != addressPort {
-				pfxlog.Logger().Debugf("advertised port [%d] in [listeners[%d].options.advertise] does not match the listening port [%d] in [listeners[%d].address].", index, advertisePort, index, addressPort)
+				pfxlog.Logger().Infof("advertised port [%d] in [listeners[%d].options.advertise] does not match the listening port [%d] in [listeners[%d].address].", index, advertisePort, index, addressPort)
 			}
 
 			return &edge_ctrl_pb.Listener{
