cf73de3cadf4199cd024d29bfbb6f2f3c56f4bea
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2024-03-08 12:28:54 -0500

    set defaults to support running router in Docker without compose

--- dist/dist-packages/linux/openziti-router/entrypoint.bash
+++ dist/dist-packages/linux/openziti-router/entrypoint.bash
@@ -10,9 +10,9 @@ set -o pipefail
 # shellcheck disable=SC1090 # default path is set by the systemd service
 source "${ZITI_ROUTER_BOOTSTRAP_BASH:-/opt/openziti/etc/router/bootstrap.bash}"
 # if no args or first arg is "run", bootstrap the router with the config file path as next arg, or default "config.yml"
-if [[ $# -eq 0 || ${1} == run ]]; then
-    bootstrap "${2:-}"
+if [ "${1:-run}" == run ]; then
+    bootstrap "${2:-config.yml}"
 fi
 
 # shellcheck disable=SC2068
-exec ziti router ${@}
+exec ziti router ${@:-run config.yml}
