2968ccccc7c94f7b3a83be3594abc51681f959d5
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-08-21 14:13:54 -0400

    changes to allow running on macos as well

--- dist/docker-images/ziti-controller/compose.test.bash
+++ dist/docker-images/ziti-controller/compose.test.bash
@@ -12,16 +12,14 @@ cleanup(){
 }
 
 portcheck(){
-	local _binds
-	_binds="$(ss -lntp | awk -v "port=${1}" '$4~ ":" port "$"')"
-	if [[ -z "${_binds}" ]]
-	then
-		return 0
-	else
-		echo -e "ERROR: Port ${1} is already bound"\
-		"\n${_binds}" >&2
-		return 1
-	fi
+    PORT="${1}"
+    if [ "$(nc -zv -G 5 localhost "$PORT" 2>&1 | grep -c 'succeeded')" -lt 1 ]; then
+		echo "port $PORT is FREE"
+        return 0
+    else
+		echo "port $PORT is already allocated! cannot continue"
+        return 1
+    fi
 }
 
 BASEDIR="$(cd "$(dirname "${0}")" && pwd)"
@@ -29,7 +27,7 @@ REPOROOT="$(cd "${BASEDIR}/../../.." && pwd)"
 cd "${REPOROOT}"
 
 : "${ZIGGY_UID:=$(id -u)}"
-: "${ZITI_GO_VERSION:=$(grep -Po '^go\s+\K\d+\.\d+(\.\d+)?$' ./go.mod)}"
+: "${ZITI_GO_VERSION:=$(grep -E '^go \d+\.\d*' "./go.mod" | cut -d " " -f2)}"
 
 export COMPOSE_FILE=\
 ./dist/docker-images/ziti-controller/compose.yml\
@@ -39,14 +37,14 @@ export COMPOSE_FILE=\
 ZIGGY_UID \
 ZITI_GO_VERSION \
 ZITI_PWD="ziggypw" \
-ZITI_CTRL_ADVERTISED_ADDRESS="ctrl1.127.21.71.0.sslip.io" \
+ZITI_CTRL_ADVERTISED_ADDRESS="ctrl1.127.0.0.1.sslip.io" \
 ZITI_CTRL_ADVERTISED_PORT="12800" \
 ZITI_ROUTER_PORT="30222" \
 ZITI_CONTROLLER_IMAGE="ziti-controller:local" \
 ZITI_ROUTER_IMAGE="ziti-router:local" \
 ZITI_ROUTER_NAME="router1"
 
-export ZITI_ROUTER_ADVERTISED_ADDRESS="${ZITI_ROUTER_NAME}.127.21.71.0.sslip.io" \
+export ZITI_ROUTER_ADVERTISED_ADDRESS="${ZITI_ROUTER_NAME}.127.0.0.1.sslip.io" \
 ZITI_ENROLL_TOKEN="/home/ziggy/.config/ziti/${ZITI_ROUTER_NAME}.jwt" \
 ZITI_ROUTER_LISTENER_BIND_PORT="${ZITI_ROUTER_PORT}"
 
@@ -57,8 +55,11 @@ do
 	portcheck "${PORT}"
 done
 
-mkdir -p ./release/amd64/linux
-go build -o ./release/amd64/linux ./...
+export GOOS="linux"
+os="$(go env GOOS)"
+arch="$(go env GOARCH)" 
+mkdir -p ./release/$arch/$os
+go build -o ./release/$arch/$os ./...
 
 ZITI_CLI_IMAGE="ziti-cli"
 ZITI_CLI_TAG="local"
@@ -102,8 +103,9 @@ ziti edge create edge-router "${ZITI_ROUTER_NAME}" -to ~ziggy/.config/ziti/"${ZI
 
 docker compose up ziti-router --detach
 
+unset GOOS
 ZITI_CTRL_EDGE_ADVERTISED_ADDRESS=${ZITI_CTRL_ADVERTISED_ADDRESS} \
 ZITI_CTRL_EDGE_ADVERTISED_PORT=${ZITI_CTRL_ADVERTISED_PORT} \
 go test -v -count=1 -tags="quickstart manual" ./ziti/cmd/edge/...
 
-cleanup
+#cleanup
