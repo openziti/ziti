44961ee61da59b1b1fe77abfcb41fccd6e046052
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2025-01-28 16:59:28 -0500

    set appropriate filemodes on unwrapped identity files

--- ziti/cmd/unwrap_identity.go
+++ ziti/cmd/unwrap_identity.go
@@ -7,7 +7,9 @@ import (
 	"github.com/spf13/cobra"
 	"io"
 	"os"
+	"runtime"
 	"strings"
+	"syscall"
 )
 
 type IdentityConfigFile struct {
@@ -57,7 +59,7 @@ func NewUnwrapIdentityFileCommand(out io.Writer, errOut io.Writer) *cobra.Comman
 
 			if strings.HasPrefix(config.ID.Cert, "pem:") {
 				data := strings.TrimPrefix(config.ID.Cert, "pem:")
-				if err := os.WriteFile(outCertFile, []byte(data), 0); err != nil {
+				if err := os.WriteFile(outCertFile, []byte(data), getFileMode(false)); err != nil {
 					_, _ = fmt.Fprintf(errOut, "error writing certificate to file [%s]: %v\n", outCertFile, err)
 					return
 				}
@@ -67,7 +69,7 @@ func NewUnwrapIdentityFileCommand(out io.Writer, errOut io.Writer) *cobra.Comman
 
 			if strings.HasPrefix(config.ID.Key, "pem:") {
 				data := strings.TrimPrefix(config.ID.Key, "pem:")
-				if err := os.WriteFile(outKeyFile, []byte(data), 0); err != nil {
+				if err := os.WriteFile(outKeyFile, []byte(data), getFileMode(true)); err != nil {
 					_, _ = fmt.Fprintf(errOut, "error writing private key to file [%s]: %v\n", outKeyFile, err)
 					return
 				}
@@ -75,14 +77,14 @@ func NewUnwrapIdentityFileCommand(out io.Writer, errOut io.Writer) *cobra.Comman
 				_, _ = fmt.Fprintf(errOut, "error writing private key to file [%s]: missing pem prefix, type is unsupported\n", outKeyFile)
 			}
 
-			if strings.HasPrefix(config.ID.Key, "pem:") {
+			if strings.HasPrefix(config.ID.CA, "pem:") {
 				data := strings.TrimPrefix(config.ID.CA, "pem:")
-				if err := os.WriteFile(outCaFile, []byte(data), 0); err != nil {
+				if err := os.WriteFile(outCaFile, []byte(data), getFileMode(false)); err != nil {
 					_, _ = fmt.Fprintf(errOut, "error writing CAs to file [%s]: %v\n", outCaFile, err)
 					return
 				}
 			} else {
-				_, _ = fmt.Fprintf(errOut, "error writing CAs to file [%s]: missing pem prefix, type is unsupported\n", outKeyFile)
+				_, _ = fmt.Fprintf(errOut, "error writing CAs to file [%s]: missing pem prefix, type is unsupported\n", outCaFile)
 			}
 		},
 	}
@@ -92,4 +94,25 @@ func NewUnwrapIdentityFileCommand(out io.Writer, errOut io.Writer) *cobra.Comman
 	cmd.Flags().StringVarP(&outCaFile, "ca", "", "", "output ca bundle file, defaults to ./<root>.ca")
 
 	return cmd
+}
+
+func getFileMode(isPrivateKey bool) os.FileMode {
+	// Default modes before umask:
+	// - Private keys: 0600 (rw-------)
+	// - Public files: 0644 (rw-r--r--)
+	mode := os.FileMode(0644)
+	if isPrivateKey {
+		mode = os.FileMode(0600)
+	}
+
+	if runtime.GOOS != "windows" {
+		// Get current umask
+		oldMask := syscall.Umask(0)
+		syscall.Umask(oldMask) // Restore original umask
+
+		// Apply umask to our default mode
+		mode &= ^os.FileMode(oldMask)
+	}
+
+	return mode
 }
