f8d23cef3c49a7fa9ffd9a1c6e3bd0b4590485ae
Paul Lorenz <paul.lorenz@netfoundry.io>
2025-03-12 15:09:03 -0400

    Only create ziti config dir on write. Fix ha quickstart test.

--- quickstart/test/ha-test.sh
+++ quickstart/test/ha-test.sh
@@ -137,7 +137,7 @@ echo "========================================================="
 ZITI_CTRL_EDGE_ADVERTISED_ADDRESS=localhost \
 ZITI_CTRL_EDGE_ADVERTISED_PORT=2001 \
 ZITI_ROUTER_NAME="router-inst1" \
-go test -tags "quickstart manual" ziti/cmd/edge/quickstart_manual_test.go ziti/cmd/edge/quickstart_shared_test.go
+go test -tags "quickstart manual" ziti/run/quickstart_manual_test.go ziti/run/quickstart_shared_test.go
 test_exit_code=$?
 
 echo "waiting for processes to exit: $pid1 $pid2 $pid3"
