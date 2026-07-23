8b513d1e9fad2e35854e54d19890124c1dba9e0d
Shawn Carey <shawn.carey@netfoundry.io>
2026-07-23 07:27:09 -0400

    set dns ip range before creating interceptor [v1.6.x] (#4129)
    * set dns ip range before creating interceptor
    
    * update changelog

--- ziti/tunnel/root.go
+++ ziti/tunnel/root.go
@@ -113,6 +113,14 @@ func rootPreRun(cmd *cobra.Command, _ []string) {
 		// let logrus do its own thing
 	}
 	util.LogReleaseVersionCheck()
+
+	// Must happen before the subcommand's RunE/Run (e.g. tproxy's interceptor construction),
+	// which reads the DNS intercept IP range to install the local route on lo.
+	dnsIpRange, _ := cmd.Flags().GetString(dnsSvcIpRangeFlag)
+	if err := intercept.SetDnsInterceptIpRange(dnsIpRange); err != nil {
+		logrus.Fatalf("invalid dns service IP range %s: %v", dnsIpRange, err)
+	}
+
 }
 
 func rootPostRun(cmd *cobra.Command, _ []string) {
@@ -142,11 +150,6 @@ func rootPostRun(cmd *cobra.Command, _ []string) {
 	}
 
 	serviceListenerGroup := intercept.NewServiceListenerGroup(interceptor, resolver)
-
-	dnsIpRange, _ := cmd.Flags().GetString(dnsSvcIpRangeFlag)
-	if err := intercept.SetDnsInterceptIpRange(dnsIpRange); err != nil {
-		log.Fatalf("invalid dns service IP range %s: %v", dnsIpRange, err)
-	}
 
 	if idDir := cmd.Flag("identity-dir").Value.String(); idDir != "" {
 		files, err := os.ReadDir(idDir)
