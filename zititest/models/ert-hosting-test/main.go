6be123621e0d3abe275455e06058dffcc9c6d05d
Paul Lorenz <paul.lorenz@netfoundry.io>
2026-09-21 11:01:32 -0400

    Restart controllers that exited when validating that components are up
    - adds chaos.EnsureUp, backported from main, which validates components are running and starts any that aren't before validating again
    - uses it for controllers in the ert-hosting and sdk-hosting models, since a 1.6 controller exits after restoring a snapshot and relies on a supervisor to return, which fablab doesn't provide
    - uses it for routers in place of the inline start that never rechecked

--- zititest/models/ert-hosting-test/main.go
+++ zititest/models/ert-hosting-test/main.go
@@ -24,7 +24,6 @@ import (
 	"path"
 	"time"
 
-	"github.com/michaelquigley/pfxlog"
 	"github.com/openziti/fablab"
 	"github.com/openziti/fablab/kernel/lib/actions"
 	"github.com/openziti/fablab/kernel/lib/actions/component"
@@ -333,7 +332,9 @@ var m = &model.Model{
 		}),
 		"sowChaos": model.Bind(model.ActionFunc(sowChaos)),
 		"validateUp": model.Bind(model.ActionFunc(func(run model.Run) error {
-			if err := chaos.ValidateUp(run, ".ctrl", 3, 15*time.Second); err != nil {
+			// A controller that restores a snapshot exits and waits for a supervisor to bring it back.
+			// There isn't one here, so EnsureUp stands in for it.
+			if err := chaos.EnsureUp(run, ".ctrl", 3, time.Minute); err != nil {
 				return err
 			}
 			err := run.GetModel().ForEachComponent(".ctrl", 3, func(c *model.Component) error {
@@ -342,11 +343,7 @@ var m = &model.Model{
 			if err != nil {
 				return err
 			}
-			if err := chaos.ValidateUp(run, ".router", 100, time.Minute); err != nil {
-				pfxlog.Logger().WithError(err).Error("validate up failed, trying to start all routers again")
-				return component.StartInParallel(".router", 100).Execute(run)
-			}
-			return nil
+			return chaos.EnsureUp(run, ".router", 100, time.Minute)
 		})),
 		"validate": model.Bind(model.ActionFunc(validateTerminators)),
 		"testIteration": model.Bind(model.ActionFunc(func(run model.Run) error {
