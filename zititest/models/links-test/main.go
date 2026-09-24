927042bbb7f51842cb71b8a193b3bcc48bc7087a
Paul Lorenz <paul.lorenz@netfoundry.io>
2026-09-23 22:30:32 -0400

    Run links-test routers on a single version
    - removes the v1.5.4 routers from links-test so every router runs the version under test

--- zititest/models/links-test/main.go
+++ zititest/models/links-test/main.go
@@ -19,6 +19,10 @@ package main
 import (
 	"embed"
 	_ "embed"
+	"os"
+	"path"
+	"time"
+
 	"github.com/michaelquigley/pfxlog"
 	"github.com/openziti/fablab"
 	"github.com/openziti/fablab/kernel/lib/actions"
@@ -40,9 +44,6 @@ import (
 	"github.com/openziti/ziti/zititest/zitilab/actions/edge"
 	"github.com/openziti/ziti/zititest/zitilab/chaos"
 	"github.com/openziti/ziti/zititest/zitilab/models"
-	"os"
-	"path"
-	"time"
 )
 
 const TargetZitiVersion = ""
@@ -101,21 +102,6 @@ var m = &model.Model{
 		model.FactoryFunc(func(m *model.Model) error {
 			return m.ForEachHost("component.router", 1, func(host *model.Host) error {
 				host.InstanceType = "c5.xlarge"
-				return nil
-			})
-		}),
-	},
-	Factories: []model.Factory{
-		model.FactoryFunc(func(m *model.Model) error {
-			return m.ForEachComponent("component.router", 1, func(c *model.Component) error {
-				if routerType, ok := c.Type.(*zitilab.RouterType); ok {
-					clone := *routerType
-					c.Type = &clone
-					// fmt.Printf("%s: %d - %s - \n", c.Id, c.ScaleIndex, routerType.Version)
-					if c.ScaleIndex >= 14 {
-						clone.Version = "v1.5.4"
-					}
-				}
 				return nil
 			})
 		}),
